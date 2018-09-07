b84ad9e43d5ab0a74a1fa5972ded0efe86b06b31
Eike Dreyer <eikedreyer@gmx.de>
2018-09-07 14:00:30 +0200

    OpenWeater REST eingebunden

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,10 +1,11 @@
 package main;
 
-import com.google.gson.Gson;
 
-import java.io.BufferedReader;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
@@ -30,14 +31,33 @@ public class App {
             url = new URL(currWeaterURL + city + "&appid=" + key + "&units=metric");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            BufferedReader br = new BufferedReader(new InputStreamReader((con.getInputStream())));
-            String output;
 
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
+            con.connect();
+            String inline = "";
+            int responscode = con.getResponseCode();
+            if(responscode !=200) {
+                throw new RuntimeException("HttpResponseCode:"+responscode);
+            } else {
+                Scanner sc = new Scanner(url.openStream());
+                while(sc.hasNext()){
+                    inline +=sc.nextLine();
+                }
+                System.out.println("\n JSON data in string format");
+                System.out.println(inline);
+                sc.close();
             }
 
+            JSONParser parser = new JSONParser();
+
+            JSONObject jobj = (JSONObject)parser.parse(inline);
+            //JSONArray jsonArray = (JSONArray) jobj.get("weather");
+            JSONObject mainObj = (JSONObject) jobj.get("main");
+            System.out.print("In " + city + " ist es " + mainObj.get("temp") + " grad.");
+            //System.out.println(mainObj.get("temp"));
+
+            /*for(int i = 0; i < jsonArray.size(); i++) {
+                JSONObject sepJobj = (JSONObject) jsonArray.get(i);
+            }*/
             con.disconnect();
 
         } catch (MalformedURLException e) {
@@ -45,6 +65,8 @@ public class App {
         } catch (ProtocolException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
             e.printStackTrace();
         }
     }
